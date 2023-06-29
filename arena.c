de31048de448980041442265010a328b08af770b
Widianto Nur F <xnaltasee@gmail.com>
2023-06-29 18:27:29 +0700

    arena.c: add reset capability

--- arena.c
+++ arena.c
@@ -1,28 +1,32 @@
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct block {
     struct block *prev;
-    size_t count;
-    size_t capa;
+    struct block *next;
+    uint32_t count, capacity;
     void *data;
 };
 
 struct allocator {
-    struct block *head; /* current block */
-    size_t block_data_size;
+    struct block *head;
+    struct block *tail;
+    struct block *curr; /* current block in use */
+    size_t data_size;
 };
 
-static struct block *block_new(size_t capa, size_t size)
+static struct block *block_new(size_t capacity, size_t size)
 {
     struct block *block;
 
-    block = calloc(1, sizeof(struct block) + size * capa);
+    block = calloc(1, sizeof(struct block) + size * capacity);
     assert(block != NULL);
 
-    block->prev = NULL;
     block->count = 0;
-    block->capa = capa;
+    block->capacity = (uint32_t)capacity;
     block->data = (unsigned char *)block + sizeof(struct block);
     return block;
 }
@@ -33,45 +37,73 @@ void allocator_setup(struct allocator *a, size_t capa, size_t size)
 
     block = block_new(capa, size);
     a->head = block;
-    a->block_data_size = size;
+    a->tail = block;
+    a->curr = block;
+    a->data_size = size;
 }
 
 void allocator_cleanup(struct allocator *a)
 {
-    struct block *head, *prev;
+    struct block *head, *next;
 
     head = a->head;
     while (head != NULL) {
-        prev = head->prev;
+        next = head->next;
         free(head);
-        head = prev;
+        head = next;
     }
 
-    a->block_data_size = 0;
+    a->head = NULL;
+    a->tail = NULL;
+    a->curr = NULL;
+    a->data_size = 0;
+}
+
+void allocator_reset(struct allocator *a)
+{
+    struct block *head, *next;
+
+    head = a->head;
+    while (head != NULL) {
+        next = head->next;
+        if (head->count > 0) {
+            memset(head->data, 0, head->count * a->data_size);
+            head->count = 0;
+        }
+        head = next;
+    }
+
+    a->curr = a->head;
 }
 
 void *allocator_malloc(struct allocator *a)
 {
     void *ptr;
-    struct block *head, *nhead;
+    struct block *curr, *nblk;
 
-    head = a->head;
-    if (head->count == head->capa) {
-        nhead = block_new(head->capa, a->block_data_size);
-        nhead->prev = head;
-        a->head = nhead;
-        head = nhead;
+    curr = a->curr;
+    if (curr->count == curr->capacity) {
+        if (curr->next != NULL) {
+            a->curr = curr->next;
+            curr = curr->next;
+        } else {
+            nblk = block_new(curr->capacity, a->data_size);
+            nblk->next = NULL;
+            nblk->prev = a->tail;
+            a->tail->next = nblk;
+            a->tail = nblk;
+            a->curr = nblk;
+            curr = nblk;
+        }
     }
 
-    ptr = (unsigned char *)head->data + head->count * a->block_data_size;
-    head->count++;
+    ptr = (unsigned char *)curr->data + curr->count * a->data_size;
+    curr->count++;
     return ptr;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-
-#include <stdio.h>
 
 struct node {
     int v;
@@ -97,7 +129,24 @@ int main(void)
     struct node *next;
     while (head != NULL) {
         next = head->next;
-        printf("%d\n", head->v);
+        head = next;
+    }
+
+    allocator_reset(&alloc);
+
+    head = allocator_malloc(&alloc);
+    head->v = 1;
+    head->next = NULL;
+
+    for (int i = 2; i <= 1024; i++) {
+        struct node *temp = allocator_malloc(&alloc);
+        temp->v = i;
+        temp->next = head;
+        head = temp;
+    }
+
+    while (head != NULL) {
+        next = head->next;
         head = next;
     }
 
